fc6525f5ad32da2f26e78d50211fa7320ba4fac8
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-10-08 19:00:29 -0400

    Added put request

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,41 +1,50 @@
-const mongoose = require('mongoose');
-const ParkingSchema = mongoose.model('Parking');
+const mongoose = require("mongoose");
+const ParkingSchema = mongoose.model("Parking");
 
-module.exports = (app) => {
-    app.post('/parking', async(req, res) => {
-        
-        console.log(req.body);
+module.exports = app => {
+  // Adding one parking space
+  app.post("/parking", async (req, res) => {
+    console.log(req.body);
 
-        try {
-            const addData = new ParkingSchema({
-                isOpen: req.body.isOpen
-            })
+    try {
+      const addData = new ParkingSchema({
+        isOpen: req.body.isOpen
+      });
 
-            await addData.save();
+      await addData.save();
 
-            return res.json({
-                msg: 'saved'
-            });
-        } catch (error) {
-            console.log(err)
-            return res.status(404).json({error});
-        }
-    });
+      return res.json({
+        msg: "saved"
+      });
+    } catch (error) {
+      console.log(err);
+      return res.status(404).json({ error });
+    }
+  });
 
-    app.get('/parking', async(req, res) => {
+  app.get("/parking", async (req, res) => {
+    try {
+      const array = [];
+      const response = await ParkingSchema.find();
 
-        try {
-            const array = [];
-            const response = await ParkingSchema.find();
-            
-            response.map(doc => {
-                array.push(doc);
-            });
+      response.map(doc => {
+        array.push(doc);
+      });
 
-            return res.json(array);
-        } catch (error) {
-            console.log(err);
-            return res.status(404).json({error});
-        }
-    });
-}
+      return res.json(array);
+    } catch (error) {
+      console.log(err);
+      return res.status(404).json({ error });
+    }
+  });
+  app.put("/api/parking/{id}", async (req, res) => {
+    try {
+      const response = await ParkingSchema.findById(req.params.id);
+      await response.update({ isOpen: !isOpen });
+      return res.status(200).json({ msg: "Switched" });
+    } catch (err) {
+      console.log(err);
+      return res.status(404).json({ err });
+    }
+  });
+};
