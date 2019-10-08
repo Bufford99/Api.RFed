a6ff5cc68cd855cb5c0815af32bcc40ccd7d3350
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-10-08 18:53:16 -0400

    Added security

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoKey: "mongodb+srv://Jose22i:vcurams22@cluster0-9evdz.mongodb.net/test?retryWrites=true&w=majority"
+}
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const app = require('express') ()
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const keys = require("./config/keys")
 app.use(bodyParser.json());
 
 require('./Models/ParkingModel');
 require('./Routes/routes') (app);
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://Jose22i:vcurams22@cluster0-9evdz.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect(keys.mongoKey);
 
 app.listen(5000);
